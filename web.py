0c5fe31d653be1f8ef26cf392fb33da212f1b298
Mizie Nordin <mizie@atsutane.net>
2021-02-13 09:02:24 +0800

    Fix port number on web.py

--- web.py
+++ web.py
@@ -42,4 +42,4 @@ def index():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80,debug=True)
+    app.run(host="0.0.0.0", port=8000,debug=True)
